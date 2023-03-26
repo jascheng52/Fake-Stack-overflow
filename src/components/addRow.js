34d0049d3a2d99d852d2e0f258e8e05eb7d3b0c7
Raymond Lui <raymond.lui@stonybrook.edu>
2023-03-25 23:57:35 -0400

    initial page

---
+++ src/components/addRow.js
@@ -0,0 +1,72 @@
+// THIS FUNCTION ADDS A ROW
+import React from 'react';
+//import deleteRows from '../components/deleteRows.js';
+// import Model from '../models/model.js';
+import timeCheck from '../components/timeCheck';
+import PropTypes from 'prop-types';
+
+AddRow.propTypes = {
+  theModel: PropTypes.object,
+  question: PropTypes.object.isRequired,
+};
+
+AddRow.propTypes = {
+
+};
+
+function getTagName(tid,theModel){
+  const tagsArr = theModel.data.tags;
+  console.log("tagsArr is " + tagsArr);
+  for (let i = 0; i < tagsArr.length; i++){
+      const id = tagsArr[i].tid;
+      if (id == tid){
+          return tagsArr[i].name;
+      }
+  }
+}
+
+function AddRow({question, theModel}) {
+  if (!question) {
+    return null; 
+  }
+  // const tableRows = questions.map((question) => {
+    const qid = question.qid;
+    const num = qid.replace(/\D/g, '');
+    console.log("questions is " + question);
+    const names = question.tagIds.map(tagId => getTagName(tagId,theModel));
+    console.log("bob is very cool " + names);
+    const dateArr = question.askDate.toString().split(" ");
+    const dateStr = question.askDate.toString();
+    const dateObj = new Date(dateStr);
+
+    let datePrint;
+    if (timeCheck.checkOverOneYear(dateObj)){
+        datePrint = dateArr[1] + " " + dateArr[2] + "," + dateArr[3] + " at " + dateArr[4];
+    }
+    else if (timeCheck.checkUnderTwentyFourHours(dateObj)){
+        datePrint = timeCheck.underTwentyFourReturn(dateObj);
+    }
+    else {
+        datePrint = dateArr[1] + " " + dateArr[2] + " at " + dateArr[4];
+    }
+
+    return (
+      <tr key={num-1} className="insertedRow" id={num-1}>
+        <td className="newCellOne">{question.ansIds.length} answers {question.views} views</td>
+        <td className="newCellTwo">
+          <div style={{ overflowWrap: "break-word" }}>{question.title}</div>
+          <div>
+            {names.map((tag) => (
+              <span key={tag} className="tagsCss">{tag}</span>
+            ))}
+          </div>
+        </td>
+        <td className="newCellFour" style={{ overflowWrap: "break-word" }}>
+          {question.askedBy} asked
+        </td>
+        <td className="newCellFive">{datePrint}</td>
+      </tr>
+    );
+}
+
+export default AddRow;
--- src/components/leftSideMenu.js
+++ src/components/leftSideMenu.js
@@ -1,30 +1,43 @@
 // This function is the left side menu
-import React from 'react';
-// import Model from '../models/model.js';
+import React, { useState } from "react";
+import QuestionPage from '../components/questionPage';
 // import hideAllHidden  from '../components/hideAllHidden.js';
 // import addRow from '../components/addRow.js';
 // import newestButton from '../components/newestButton.js';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-// LeftSideMenu.propTypes = {
-//     theModel: PropTypes.object.isRequired,
-// };
+LeftSideMenu.propTypes = {
+    theModel: PropTypes.object,
+};
 
-export default function LeftSideMenu(){
-
+export default function LeftSideMenu({ theModel }) {
+    const [showQuestionPage, setShowQuestionPage] = useState(false);
+  
+    const handleQuestionClick = () => {
+      setShowQuestionPage(true);
+    };
+  
+    if (showQuestionPage) {
+      return <QuestionPage theModel={theModel} />;
+    }
+  
     return (
-        <>
-        <div className = "leftSide">
-        <table className ="left-table">
+      <>
+        <div className="leftSide">
+          <table className="left-table">
             <tr>
-            <td height='100' id="tableSide"><h2> Question </h2></td>   
+              <td height="100" id="tableSide" onClick={handleQuestionClick}>
+                <h2>Question</h2>
+              </td>
             </tr>
             <tr>
-            <td height='100' id="tagsSide"><h2> Tags </h2></td>   
+              <td height="100" id="tagsSide">
+                <h2> Tags </h2>
+              </td>
             </tr>
             <tr></tr>
-        </table>
+          </table>
         </div>
-        </>
+      </>
     );
-}
+  }
