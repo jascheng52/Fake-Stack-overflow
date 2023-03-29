88893a9d54024fa3699b93bf3e390a3dd3a4b544
Raymond Lui <raymond.lui@stonybrook.edu>
2023-03-29 14:04:12 -0400

    removed comments

--- src/components/hideAllHidden.js
+++
@@ -1,8 +0,0 @@
-// this function is to make parts of the code hidden
-
-export default function HideAllHidden () {
-  const hiddenPages = document.getElementsByClassName('hidden')
-  for (let i = 0; i < hiddenPages.length; i++) {
-    hiddenPages[i].style.display = 'none'
-  }
-}
